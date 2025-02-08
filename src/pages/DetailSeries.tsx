677cc6b9d199ff6577596cfa1fb2bafde0a91161
songwon-sung <swsung91@gmail.com>
2025-02-08 18:13:20 +0900

    fix: data props로 전달

--- src/pages/DetailSeries.tsx
+++ src/pages/DetailSeries.tsx
@@ -1,21 +1,28 @@
 import DetailIntroBox from "../components/common/DetailIntroBox";
 import SeasonBox from "../components/common/SeasonBox";
 import { tvAPI } from "../api/tv";
-// import SeriesThumbnail from "../components/common/SeriesThumbnail";
+import { useEffect, useState } from "react";
 
 export default function DetailSeries() {
-  const fetchSeries = async () => {
-    try {
-      const series = await tvAPI.getSeries(96102);
-      console.log(series);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchSeries();
+  const [contentData, setContentData] =
+    useState<OnTheAirTvSerieseResultsType>();
+
+  useEffect(() => {
+    const fetchSeries = async () => {
+      try {
+        const series = await tvAPI.getSeries(96102);
+        console.log(series);
+        setContentData(series);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSeries();
+  }, []);
+
   return (
     <div>
-      <DetailIntroBox />
+      <DetailIntroBox data={contentData} />
       <SeasonBox />
       {/* 추천 */}
       {/* 유사한 작품 */}
