26abffa15faf53f16ff48e6a6d6b0f3e9d1bf49f
songwon-sung <swsung91@gmail.com>
2025-02-08 18:12:45 +0900

    fix: 태그 props 타입 수정

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -6,7 +6,7 @@ export default function Tag({
   onClick,
   isSelected,
 }: {
-  children: string;
+  children?: string | number;
   onClick?: () => void;
   isSelected?: boolean;
 }) {
@@ -23,7 +23,8 @@ export default function Tag({
         className={`desktop:flex tablet:flex mobile:hidden h-[30px] items-center px-[10px]  rounded-[10px] font-light text-white03 ${
           isSelected ? "bg-gray01 border-0" : "border-[1px] border-white03"
         }`}
-        onClick={handleClick}>
+        onClick={handleClick}
+      >
         {children}
         {isSelected && <img src={TagCancelIcon} className="ml-[10px]"></img>}
       </div>
@@ -33,7 +34,8 @@ export default function Tag({
         className={`desktop:hidden tablet:hidden mobile:flex h-[24px] items-center px-[8px] rounded-[8px] font-light text-[14px] text-white03 ${
           isSelected ? "bg-gray01 border-0" : "border-[1px] border-white03"
         }`}
-        onClick={handleClick}>
+        onClick={handleClick}
+      >
         {children}
         {isSelected && (
           <img src={TagCancelIcon} className="ml-[10px] w-2 h-2"></img>
