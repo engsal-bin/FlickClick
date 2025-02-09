c52884b497ee11e0d0661eee1b3a696825c88137
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-09 20:10:27 +0900

    [DELETE] 불필요한 파일 및 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ import DetailSeriesNoSeason from "./pages/DetailSeriesNoSeason";
 import DetailMovie from "./pages/DetailMovie";
 import Trailers from "./components/common/Trailers";
 import TrailerModal from "./components/common/TrailerModal";
-import Search from "./pages/Search";
 
 export default function App() {
   return (
