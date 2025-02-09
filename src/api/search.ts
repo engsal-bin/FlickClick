cc7054f9b639e4a52c521767d8b8661495d2ac25
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-10 00:48:01 +0900

    [FEAT] 멀티 검색 API 추가

--- src/api/search.ts
+++ src/api/search.ts
@@ -10,20 +10,30 @@ const keywordSearch = async (keyword: string, page = 1) => {
     console.error("API 호출 중 오류 발생:", error);
   }
 };
-const movieSearch = async (keyword: string, page = 1) => {
+const movieSearch = async (keyword: string, language = "ko-KR", page = 1) => {
   try {
     const response = await axiosInstance.get(`/search/movie`, {
-      params: { query: keyword, page },
+      params: { query: keyword, language, page },
     });
     return response.data;
   } catch (error) {
     console.error("API 호출 중 오류 발생:", error);
   }
 };
-const tvSearch = async (keyword: string, page = 1) => {
+const tvSearch = async (keyword: string, language = "ko-KR", page = 1) => {
   try {
     const response = await axiosInstance.get(`/search/tv`, {
-      params: { query: keyword, page },
+      params: { query: keyword, language, page },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+  }
+};
+const multiSearch = async (keyword: string, language = "ko-KR", page = 1) => {
+  try {
+    const response = await axiosInstance.get(`/search/multi`, {
+      params: { query: keyword, language, page },
     });
     return response.data;
   } catch (error) {
@@ -35,4 +45,5 @@ export const searchAPI = {
   keywordSearch,
   movieSearch,
   tvSearch,
+  multiSearch,
 };
