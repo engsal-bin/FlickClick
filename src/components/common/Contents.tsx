84acc09cda181a6a5621f4ad1e8e3f87b3a446d1
engsal <imaria0218@gmail.com>
2025-02-09 23:27:04 +0900

    [FEAT] 메인배너에서 상세페이지로 이동

--- src/components/common/Contents.tsx
+++ src/components/common/Contents.tsx
@@ -1,67 +1,94 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { mediaTypeToPathName } from "../../constants/path";
 
 interface ChildProps {
-  imgSrc: string[];
-  children?: React.ReactNode;
   to: string;
-  showMore: boolean;
+  showMore?: boolean;
+  trendingData?: BasicType[];
+  children?: React.ReactNode;
 }
-export default function Contents(props: ChildProps) {
-  const imgSrc = props.imgSrc;
 
-  return (
-    <>
-      {/* tablet 이상 */}
-      <div className="hidden tablet:flex flex-col justify-between w-full px-[50px]  ">
-        <div className="flex items-baseline justify-between">
-          <p className="text-white01 font-bold text-[24px] mb-[30px]">
-            {props.children}
-          </p>
-          {props.showMore && (
-            <Link to={props.to} className="text-white03 text-[20px]">
-              더보기
-            </Link>
-          )}
+export default function Contents({
+  to,
+  showMore,
+  trendingData,
+  children,
+}: ChildProps) {
+  const imgSrc = trendingData?.map((item) => item.poster_path);
+
+  const path =
+    trendingData?.map((item) =>
+      item.media_type && mediaTypeToPathName[item.media_type as "movie" | "tv"]
+        ? `/${mediaTypeToPathName[item.media_type as "movie" | "tv"]}/${
+            item.id
+          }`
+        : ""
+    ) ?? [];
+
+  const navigate = useNavigate();
+
+  if (imgSrc)
+    return (
+      <>
+        {/* tablet 이상 */}
+        <div className="hidden tablet:flex flex-col justify-between w-full px-[50px]  ">
+          <div className="flex items-baseline justify-between">
+            <p className="text-white01 font-bold text-[24px] mb-[30px]">
+              {children}
+            </p>
+            {showMore && (
+              <Link to={to} className="text-white03 text-[20px]">
+                더보기
+              </Link>
+            )}
+          </div>
+
+          <div className="flex justify-between overflow-y-auto ">
+            {imgSrc.map((_, index) => {
+              return (
+                <img
+                  className="w-[200px] h-[265px] border-[1px] rounded-[8px] border-gray01"
+                  src={imgSrc[index]}
+                  key={index}
+                  onClick={() => {
+                    // 이미지 클릭 시 해당 경로로 이동
+                    navigate(path[index]);
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
 
-        <div className="flex justify-between overflow-y-auto ">
-          {imgSrc.map((_, index) => {
-            return (
-              <img
-                className="w-[200px] h-[265px] border-[1px] rounded-[8px] border-gray01"
-                src={imgSrc[index]}
-                key={index}
-              />
-            );
-          })}
-        </div>
-      </div>
+        {/* mobile 전용 */}
+        <div className="tablet:hidden flex flex-col justify-between w-full px-[10px] ">
+          <div className="flex items-baseline justify-between">
+            <p className="text-white01 font-bold text-[18px] mb-[30px]">
+              {children}
+            </p>
+            {showMore && (
+              <Link to={to} className="text-white03 text-[12px]">
+                더보기
+              </Link>
+            )}
+          </div>
 
-      {/* mobile 전용 */}
-      <div className="tablet:hidden flex flex-col justify-between w-full px-[10px] ">
-        <div className="flex items-baseline justify-between">
-          <p className="text-white01 font-bold text-[18px] mb-[30px]">
-            {props.children}
-          </p>
-          {props.showMore && (
-            <Link to={props.to} className="text-white03 text-[12px]">
-              더보기
-            </Link>
-          )}{" "}
+          <div className="flex overflow-y-auto">
+            {imgSrc.map((_, index) => {
+              return (
+                <img
+                  className="w-[200px] h-[265px] border-[1px] rounded-[8px] border-gray01"
+                  src={imgSrc[index]}
+                  key={index}
+                  onClick={() => {
+                    navigate(path[index]);
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
-
-        <div className="flex overflow-y-auto">
-          {imgSrc.map((_, index) => {
-            return (
-              <img
-                className="w-[200px] h-[265px] border-[1px] rounded-[8px] border-gray01"
-                src={imgSrc[index]}
-                key={index}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from "react";
 
 import { commonAPI } from "../api/common.ts";
+import { movieAPI } from "../api/movie.ts";
+import { tvAPI } from "../api/tv.ts";
 import Banner from "../components/common/Banner";
 import Contents from "../components/common/Contents";
 import MainThumbnail from "../components/common/MainThumbnail";
-import { tvAPI } from "../api/tv.ts";
-import { movieAPI } from "../api/movie.ts";
 
 export default function Main() {
-  const [trendImgSrc, setTrendImgSrc] = useState<BasicType[]>([]);
-  const [newUpdateImgSrc, setNewUpateImgSrc] = useState<BasicType[]>([]);
-  const [upcomingImgSrc, setUpcomingImgSrc] = useState<BasicType[]>([]);
+  const [trendingData, setTrendingData] = useState<BasicType[]>([]);
+  const [newUpdateData, setNewUpdateData] = useState<BasicType[]>([]);
+  const [upComingData, setUpcomingData] = useState<BasicType[]>([]);
 
   useEffect(() => {
-    // 인기 급상승
-    const fetchTrendAll = async () => {
+    const fetchAllData = async () => {
       try {
-        const trend = await commonAPI.getTrendingAll(1);
-        setTrendImgSrc(
+        const [trend, onTheAirTvSeriese, nowPlayingMovie, upcoming] =
+          await Promise.all([
+            commonAPI.getTrendingAll(1),
+            tvAPI.getOnTheAirTvSeriese(),
+            movieAPI.getNowPlayingMovie(),
+            movieAPI.getUpComingMovie(),
+          ]);
+
+        setTrendingData(
           trend.results
             .map((item: TrendingAllResultsType) => ({
               poster_path:
@@ -29,16 +35,6 @@ export default function Main() {
             }))
             .slice(0, 20)
         );
-      } catch (error) {
-        console.error("Error fetching upcoming movies:", error);
-      }
-    };
-
-    // 신규 업데이트
-    const fetchNewUpdate = async () => {
-      try {
-        const onTheAirTvSeriese = await tvAPI.getOnTheAirTvSeriese();
-        const nowPlayingMovie = await movieAPI.getNowPlayingMovie();
 
         const combinedArr = [
           ...onTheAirTvSeriese.results.map(
@@ -69,25 +65,16 @@ export default function Main() {
           .sort((a, b) => b.popularity - a.popularity)
           .slice(0, 20);
 
-        setNewUpateImgSrc(
+        setNewUpdateData(
           sortedSrc.map((item) => ({
             poster_path: item.poster_path,
             id: item.id,
             title: item.title,
             media_type: item.media_type,
-          }))
+          })) as BasicType[]
         );
-      } catch (error) {
-        console.error("Error fetching new updates:", error);
-      }
-    };
 
-    // 공개 예정
-    const fetchUpcomingMovies = async () => {
-      try {
-        const upcoming = await movieAPI.getUpComingMovie();
-
-        setUpcomingImgSrc(
+        setUpcomingData(
           upcoming.results.map((item: UpComingMovieResultsType) => ({
             poster_path:
               "https://image.tmdb.org/t/p/w220_and_h330_face" +
@@ -98,44 +85,27 @@ export default function Main() {
           }))
         );
       } catch (error) {
-        console.error("Error fetching upcoming movies:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchTrendAll();
-    fetchNewUpdate();
-    fetchUpcomingMovies();
+    fetchAllData();
   }, []);
+
   return (
     <div className="flex flex-col justify-between items-center mb-[100px] desktop:gap-[50px] tablet:gap-[40px] mobile:gap-[30px] text-white bg-black">
       <MainThumbnail />
       <Banner />
-      <Contents
-        to="/popular"
-        showMore
-        imgSrc={trendImgSrc.map((item) => item.poster_path)}
-      >
+      <Contents to="/popular" showMore trendingData={trendingData}>
         인기 급상승
       </Contents>
-      <Contents
-        to="/newupdate"
-        showMore
-        imgSrc={newUpdateImgSrc.map((item) => item.poster_path)}
-      >
+      <Contents to="/newupdate" showMore trendingData={upComingData}>
         신규 업데이트
       </Contents>
-      <Contents
-        to="/upcomings"
-        showMore
-        imgSrc={upcomingImgSrc.map((item) => item.poster_path)}
-      >
+      <Contents to="/upcomings" showMore trendingData={newUpdateData}>
         공개 예정
       </Contents>
-      <Contents
-        to=""
-        showMore={false}
-        imgSrc={trendImgSrc.map((item) => item.poster_path)}
-      >
+      <Contents to="" showMore={false}>
         새해에 봐야하는 ☀️
       </Contents>
     </div>
