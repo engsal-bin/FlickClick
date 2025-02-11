e5103ffd75ddcb0de5ac8d4b0c9e8e187dbf64d6
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-11 15:49:52 +0900

    [DESIGN] 장르 페이지 연도 선택 체크박스 디자인 수정

--- src/components/genresPage/YearsList.tsx
+++ src/components/genresPage/YearsList.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 type CheckedState = {
   [key: string]: boolean;
 };
@@ -16,19 +14,21 @@ export default function YearsList({
   return (
     <div
       className="w-[124px] flex flex-col justify-start items-start
-     text-[13px] font-light gap-[10px]"
-    >
+     text-[13px] font-light gap-[10px]">
       <div className="flex justify-center items-center gap-[15px]">
         <input
           type="checkbox"
           id="2025-checkbox"
           checked={checked["2025년"]}
           onChange={() => onCheckboxChange("2025년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2025년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2025년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2025년</label>
       </div>
@@ -38,11 +38,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2024년"]}
           onChange={() => onCheckboxChange("2024년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2024년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2024년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2024년</label>
       </div>
@@ -52,11 +55,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2023년"]}
           onChange={() => onCheckboxChange("2023년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2023년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2023년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2023년</label>
       </div>
@@ -66,11 +72,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2022년"]}
           onChange={() => onCheckboxChange("2022년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2022년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2022년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2022년</label>
       </div>
@@ -80,11 +89,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2021년"]}
           onChange={() => onCheckboxChange("2021년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2021년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2021년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2021년</label>
       </div>
@@ -94,11 +106,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2020년"]}
           onChange={() => onCheckboxChange("2020년")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2020년"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2020년"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2020년</label>
       </div>
@@ -108,11 +123,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2010년대"]}
           onChange={() => onCheckboxChange("2010년대")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2010년대"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2010년대"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2010년대</label>
       </div>
@@ -122,11 +140,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["2000년대"]}
           onChange={() => onCheckboxChange("2000년대")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["2000년대"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["2000년대"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">2000년대</label>
       </div>
@@ -136,11 +157,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["1990년대"]}
           onChange={() => onCheckboxChange("1990년대")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["1990년대"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["1990년대"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">1990년대</label>
       </div>
@@ -150,11 +174,14 @@ export default function YearsList({
           id="2025-checkbox"
           checked={checked["1980년대"]}
           onChange={() => onCheckboxChange("1980년대")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
             checked["1980년대"]
-              ? "bg-main border-white01"
+              ? "bg-main border-none"
               : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          } transition-colors ease-in-out after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked["1980년대"] ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="years-checkbox">1980년대</label>
       </div>
