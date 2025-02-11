0e9b8a19db206f4a9de65603e1a5293ebeae90fb
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-11 15:55:08 +0900

    [FEAT & DESIGN] 장르 페이지 사용할 수 있는 서비스 아이콘 패칭 및 디자인 수정

--- src/components/OttIcon.tsx
+++ src/components/OttIcon.tsx
@@ -1,18 +1,19 @@
 interface OttIconProps {
   src: string; // 이미지 경로
-  isSelected: boolean; // 선택 여부
-  onClick: () => void; // 클릭 이벤트
+  isSelected?: boolean; // 선택 여부
+  onClick?: () => void; // 클릭 이벤트
   alt: string; // 대체 텍스트
 }
 
 const OttIcon = ({ src, isSelected, onClick, alt }: OttIconProps) => {
   return (
     <img
-      src={src}
+      src={`https://image.tmdb.org/t/p/original/${src}`}
       onClick={onClick}
       className={
-        `cursor-pointer ${isSelected ? "opacity-100" : "opacity-30"}` +
-        "object-cover"
+        `cursor-pointer ${
+          isSelected ? "opacity-100" : "opacity-30"
+        } w-[40px] h-[40px]` + "object-cover rounded-[8px]"
       }
       alt={alt}
     />
