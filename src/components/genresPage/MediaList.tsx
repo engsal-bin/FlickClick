b219d199592544f4fdae51e4b032cd00ef7f0545
ddongguri-bing <ckdalssnsk@naver.com>
2025-02-11 15:26:34 +0900

    [FEAT] 장르 페이지 타입 선택 디자인 수정 및 체크 표시 추가

--- src/components/genresPage/MediaList.tsx
+++ src/components/genresPage/MediaList.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 type CheckedState = {
   [key: string]: boolean;
 };
@@ -15,20 +13,21 @@ export default function MediaList({
 }: MediaListProps) {
   return (
     <div
-      className="w-[124px] flex flex-col justify-start items-start 
-    font-light text-[13px] gap-[10px]"
-    >
+      className="w-[124px] flex flex-col justify-start items-start
+    font-light text-[13px] gap-[10px]">
       <div className="flex justify-center items-center gap-[15px]">
         <input
           type="checkbox"
           id="series-checkbox"
           checked={checked.series}
           onChange={() => onCheckboxChange("series")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
-            checked.series
-              ? "bg-main border-white01"
-              : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
+            checked.series ? "bg-main border-none" : "bg-black border-gray-400"
+          } transition-colors ease-in-out
+            after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked.series ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="series-checkbox">시리즈</label>
       </div>
@@ -38,11 +37,13 @@ export default function MediaList({
           id="movies-checkbox"
           checked={checked.movies}
           onChange={() => onCheckboxChange("movies")}
-          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer ${
-            checked.movies
-              ? "bg-main border-white01"
-              : "bg-black border-gray-400"
-          } transition-colors ease-in-out`}
+          className={`w-[16px] h-[16px] border-2 rounded-[3px] appearance-none cursor-pointer relative ${
+            checked.movies ? "bg-main border-none" : "bg-black border-gray-400"
+          } transition-colors ease-in-out
+            after:content-[''] after:absolute after:left-[5px] after:top-[2px]
+            after:w-[6px] after:h-[10px] after:border-white
+            after:border-r-2 after:border-b-2 after:rotate-45
+            ${checked.movies ? "after:block" : "after:hidden"}`}
         />
         <label htmlFor="movies-checkbox">영화</label>
       </div>
