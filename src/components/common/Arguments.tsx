12933edcb979ecb46e8e853f8b879fc492a8e07e
naehyun <scott253@naver.com>
2025-03-19 16:31:53 +0900

    feat: 컴포넌트 생성

---
+++ src/components/common/Arguments.tsx
@@ -0,0 +1,29 @@
+import Argument from "./Argument";
+import InputTextarea from "./InputTextarea";
+export default function Arguments() {
+  const argumentContens = [
+    {
+      id: "1",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, .",
+      author: "김예빈",
+      date: "2024.10.23,오전 03:26",
+    },
+    {
+      id: "2",
+      content:
+        "이 영화 정말 재미있습니다! 다른 사람들도 꼭 봐야 할 영화인 것 같아요. 특별히 연출이 좋았어요.",
+      author: "이철수",
+      date: "2024.10.23,오전 03:26",
+    },
+  ];
+  return (
+    <div>
+      <InputTextarea reviewOrArgumentOrOpinion={"argument"} />
+      {/* 토론리스트 */}
+      {argumentContens.map((argumentConten) => (
+        <Argument argumentConten={argumentConten} />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/common/InputTextarea.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+import sendIcon from "../../assets/icon/send.svg";
+import sendBlueIcon from "../../assets/icon/sendBlue.svg";
+import { movieAPI } from "../../api/movie";
+import { tvAPI } from "../../api/tv";
+export default function InputTextarea({
+  reviewOrArgumentOrOpinion,
+}: {
+  reviewOrArgumentOrOpinion: "review" | "argument" | "opinion";
+}) {
+  const content = useRef<HTMLTextAreaElement | null>(null); // ref 객체
+
+  const [text, setText] = useState("");
+  const [isSend, setIsSend] = useState(false);
+  const [placeHolder, setPlaceHolder] = useState("");
+
+  const fn = async () => {
+    await movieAPI.postMovieReview(
+      "m",
+      text,
+      "1be395d3-dd4b-4c68-b90e-738583c1f0e4",
+      "Movie"
+    );
+    await tvAPI.postEpisodeReview(
+      "e",
+      text,
+      "1be395d3-dd4b-4c68-b90e-738583c1f0e4",
+      "Episode"
+    );
+  };
+
+  useEffect(() => {
+    text ? setIsSend(true) : setIsSend(false);
+    if (reviewOrArgumentOrOpinion === "review") {
+      setPlaceHolder("리뷰를 입력해주세요");
+    } else if (reviewOrArgumentOrOpinion === "argument") {
+      setPlaceHolder("토론 주제를 입력하다!");
+    } else {
+      setPlaceHolder("의견를 입력하세요");
+    }
+  }, [text]);
+  return (
+    <div className="flex justify-between mb-[30px] tablet:px-[30px] mobile:px-[10px]  tablet:h-[86px] mobile:h-[59px] border border-gray02 rounded-[10px]">
+      <textarea
+        onChange={(e) => setText(e.target.value)}
+        placeholder={placeHolder}
+        className="outline-none w-full h-full pt-[31px] mobile:pt-[20px] bg-black tablet tablet:text-[20px] mobile:text-[16px] placeholder:text-gray03 text-gray03  resize-none"
+      ></textarea>
+      <button>
+        <img
+          onClick={() => {
+            fn();
+          }}
+          src={isSend ? sendBlueIcon : sendIcon}
+          alt="입력 전송 버튼"
+          className="tablet:w-[26px] mobile:w-[16px]"
+        />
+      </button>
+    </div>
+  );
+}
