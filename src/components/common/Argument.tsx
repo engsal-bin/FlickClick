a348be564709a8f82df8a7a8e479b3da7978a845
naehyun <scott253@naver.com>
2025-03-19 16:31:22 +0900

    feat:컴포넌트 분리

--- src/components/common/Argument.tsx
+++ src/components/common/Argument.tsx
@@ -1,73 +1,84 @@
+import InputTextarea from "./InputTextarea";
+import arrowRight from "../../assets/icon/arrow/arrowRight.svg";
+import arrowBottom from "../../assets/icon/arrow/arrowBottom.svg";
+import { useState } from "react";
+import ArgumentReview from "./ArgumentReview";
 
-import sendIcon from "../../assets/icon/send.svg"
-import arrowRight from "../../assets/icon/arrow/arrowRight.svg"
-import arrowBottom from "../../assets/icon/arrow/arrowBottom.svg"
-import { useState } from "react"
-export default function Argument() {
-  const [isArgumentToggleOpen, setIsArgumentToggleOpen] = useState(false)
+export default function Argument({
+  argumentConten,
+}: {
+  argumentConten: ReviewType;
+}) {
+  const [isArgumentToggleOpen, setIsArgumentToggleOpen] = useState(false);
+  // 더미 토론 데이터 예시
+  const reviews = [
+    {
+      id: "1",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, commodi eum dolore. Voluptate, ad repellendus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, commodi eum dolore. Voluptate, ad repellendus.",
+      author: "김예빈",
+      date: "2024.10.22",
+      isMine: true,
+    },
+    {
+      id: "2",
+      content:
+        "이 영화 정말 재미있습니다! 다른 사람들도 꼭 봐야 할 영화인 것 같아요. 특별히 연출이 좋았어요.",
+      author: "이철수",
+      date: "2024.10.23",
+      isMine: false,
+    },
+  ];
   return (
-    <div>
-        <div className="flex justify-between mb-[30px] tablet:px-[30px] mobile:px-[10px] items-center tablet:h-[86px] mobile:h-[59px] border border-gray02 rounded-[10px]">
-            <input type="text" placeholder="토론 주제를 입력하세요" className="bg-black tablet:text-[20px] mobile:text-[16px] placeholder:text-gray03"/>
-            <button>
-                <img src={sendIcon} alt="입력 전송 버튼" className="tablet:w-[26px] mobile:w-[16px]"/>
-            </button>
+    <div className="flex flex-col gap-[20px] tablet:px-[20px] tablet:py-[30px] mobile:px-[10px] mobile:py-[20px] rounded-[10px] border border-gray03 mb-5">
+      {/* 토론 */}
+      <div className="h-auto flex justify-between tablet:flex-row mobile:flex-col tablet:gap-[20px] mobile:gap-[10px]">
+        <div className="flex items-center h-auto">
+          <img
+            src=""
+            className="bg-white h-[45px] aspect-square rounded-full"
+          />
+          <p className="text-white01 text-bold text-[18px] ml-[13px]">
+            {argumentConten.content}
+          </p>
         </div>
-        {/* 토론리스트 */}
-        <div className="flex flex-col gap-[20px] tablet:px-[20px] tablet:py-[30px] mobile:px-[10px] mobile:py-[20px] rounded-[10px] shadow-sm shadow-white01">
-                {/* 토론 */}
-                <div className="h-auto flex justify-between tablet:flex-row mobile:flex-col tablet:gap-[20px] mobile:gap-[10px]">
-                    <div className="h-auto flex items-center">
-                        <img src="" className="bg-white h-[45px] aspect-square rounded-full"/>
-                        <p className="text-white01 text-bold text-[18px] ml-[13px]">조금 긴 토론 주제입니다</p>
-                    </div>
-                    <div className="text-gray03 flex items-center gap-[30px] mobile:justify-between">
-                        {!isArgumentToggleOpen && (<><p className="mobile:hidden">3</p>
-                        <div>
-                            <p>2025-01-09, 오전 03:26</p>
-                            <p>작성자: <span>예빈</span></p>
-                        </div></>)}
-                        <button onClick={() => setIsArgumentToggleOpen(!isArgumentToggleOpen)}>
-                            <img src={isArgumentToggleOpen ? arrowBottom : arrowRight} alt="토론 펼치기 버튼" />
-                        </button>
-                    </div>
-                </div>
-                {/* 토론의 댓글 */}
-                {isArgumentToggleOpen && (<div>
-                    <hr className="border border-gray03 mb-[30px]"/>
-                    <div className="tablet:px-[20px] mobile:px-[5px] flex flex-col tablet:gap-[30px] mobile:gap-[20px]">
-                        <div className="h-auto flex flex-col tablet:gap-[20px] mobile:gap-[10px]">
-                            {/* 타인의 의견 */}
-                            <div className="min-h-[77px w-full flex">
-                                <div className="desktop:max-w-[894px] tablet:max-w-[558px] bg-gray01 rounded-[10px] w-auto h-full flex jutify-between items-center px-[20px] py-[15px]">
-                                    <img src="" className="bg-white h-[45px] aspect-square rounded-full"/>
-                                    <div className="ml-[15px]">
-                                        <p className="text-white01 tablet:text-[18px] mobile:text-[14px] font-bold">남이 어쩌구 어쩌구 의견 어쩌구 달아아하항앙dekekfkddjeejjjjj어ㅓ헣어ㅏ홈아ㅓㄴ화ㅓㄷ고 ㅣ솓시ㅕ폳지ㅓㅏ솓조깆ㄷㅎ셔ㅑㄷㅈㅎ시ㅗㅈㄷ시ㅓㅏㅚㅏㅓ봎다ㅣㅓ</p>
-                                        <p className="tablet:text-[14px] mobile:text-[12px]"><span className="text-white02 mr-[10px]">예빈</span><span className="text-white03">2025-01-10</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* 나의 의견 */}
-                            <div className="min-h-[77px] flex w-full justify-end">
-                                <div className="desktop:max-w-[894px] tablet:max-w-[558px] bg-main30 text-black01 rounded-[10px] w-auto h-full flex jutify-between items-center px-[20px] py-[15px]">
-                                    <img src="" className="bg-white h-[45px] aspect-square rounded-full"/>
-                                    <div className="ml-[15px]">
-                                        <p className="tablet:text-[18px] mobile:text-[14px] font-bold">내가 어쩌구 저꾸자아후 삐리빠리 뽕뽕 어쩌구 어쩌구 의견 어쩌구 달아아하항앙</p>
-                                        <p className="tablet:text-[14px] mobile:text-[12px]"><span className="mr-[10px]">예빈</span><span>2025-01-10</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div className="text-gray03 flex items-center gap-[30px] mobile:justify-between">
+          {!isArgumentToggleOpen && (
+            <>
+              <p className="mobile:hidden">3</p>
+              <div>
+                <p>{argumentConten.date}</p>
+                <p>
+                  작성자: <span>{argumentConten.author}</span>
+                </p>
+              </div>
+            </>
+          )}
+          <button
+            onClick={() => setIsArgumentToggleOpen(!isArgumentToggleOpen)}
+          >
+            <img
+              src={isArgumentToggleOpen ? arrowBottom : arrowRight}
+              alt="토론 펼치기 버튼"
+            />
+          </button>
+        </div>
+      </div>
+      {/* 토론의 댓글 */}
+      {isArgumentToggleOpen && (
+        <div>
+          <hr className="border border-gray03 mb-[30px]" />
+          <div className="tablet:px-[20px] mobile:px-[5px] flex flex-col tablet:gap-[30px] mobile:gap-[20px]">
+            <div className="h-auto flex flex-col tablet:gap-[20px] mobile:gap-[10px]">
+              {reviews.map((review) => (
+                <ArgumentReview review={review} />
+              ))}
+            </div>
 
-                        <div className="flex justify-between tablet:px-[30px] mobile:px-[10px] items-center tablet:h-[86px] mobile:h-[59px] border border-gray02 rounded-[10px]">
-                            <input type="text" placeholder="의견를 입력하세요" className="bg-black tablet:text-[20px] mobile:text-[16px] placeholder:text-gray03"/>
-                            <button>
-                                <img src={sendIcon} alt="입력 전송 버튼" className="tablet:w-[26px] mobile:w-[16px]"/>
-                            </button>
-                        </div>
-                    </div>
-                </div>)}
+            <InputTextarea reviewOrArgumentOrOpinion="opinion" />
+          </div>
         </div>
+      )}
     </div>
-  )
+  );
 }
--- src/components/common/Reviews.tsx
+++ src/components/common/Reviews.tsx
@@ -1,93 +1,34 @@
-import { twMerge } from "tailwind-merge";
-import sendIcon from "../../assets/icon/send.svg";
-import { useState } from "react";
+import InputTextarea from "./InputTextarea";
+import Review from "./Review";
 
 export default function Reviews() {
   // 더미 댓글 데이터 예시
   const reviews = [
     {
       id: "1",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, commodi eum dolore. Voluptate, ad repellendus.",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, commodi eum dolore. Voluptate, ad repellendus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic deleniti necessitatibus quam maxime eveniet nemo, ea natus tempora aspernatur impedit in consectetur voluptas, commodi eum dolore. Voluptate, ad repellendus.",
       author: "김예빈",
       date: "2024.10.22",
     },
     {
       id: "2",
-      content: "이 영화 정말 재미있습니다! 다른 사람들도 꼭 봐야 할 영화인 것 같아요. 특별히 연출이 좋았어요.",
+      content:
+        "이 영화 정말 재미있습니다! 다른 사람들도 꼭 봐야 할 영화인 것 같아요. 특별히 연출이 좋았어요.",
       author: "이철수",
       date: "2024.10.23",
     },
     // 추가적인 리뷰를 넣을 수 있음
   ];
 
-  // 각 댓글에 대해 '더보기' 상태
-  const [viewMoreStates, setViewMoreStates] = useState(
-    reviews.reduce((acc, review) => {
-      acc[review.id] = false; // 모든 댓글의 기본 상태
-      return acc;
-    }, {} as Record<string, boolean>)
-  );
-
-  const toggleViewMore = (id: string) => {
-    setViewMoreStates((prevState) => ({
-      ...prevState,
-      [id]: !prevState[id],
-    }));
-  };
-
   return (
     <>
       <div>
-        <div className="flex justify-between mb-[30px] tablet:px-[30px] mobile:px-[10px] items-center tablet:h-[86px] mobile:h-[59px] border border-gray02 rounded-[10px]">
-          <input
-            type="text"
-            placeholder="리뷰를 입력하세요"
-            className="bg-black tablet:text-[20px] mobile:text-[16px] placeholder:text-gray03"
-          />
-          <button>
-            <img
-              src={sendIcon}
-              alt="입력 전송 버튼"
-              className="tablet:w-[26px] mobile:w-[16px]"
-            />
-          </button>
-        </div>
-
+        <InputTextarea reviewOrArgumentOrOpinion={"review"} />
         {/* 리뷰리스트 */}
         <div className="flex flex-col gap-[20px]">
-          {reviews.map((review) => (
-            <div key={review.id} className="h-auto flex flex-col gap-[15px]">
-              <p
-                className={twMerge(
-                  `text-white01 tablet:text-[18px] mobile:text-[14px] ${
-                    !viewMoreStates[review.id] && "line-clamp-5"
-                  }`)
-                }
-              >
-                {review.content}
-                <button className="inline text-gray03">
-                  <span onClick={() => toggleViewMore(review.id)}>
-                    {viewMoreStates[review.id] ? "접기" : "더보기"}
-                  </span>
-                </button>
-              </p>
-
-              <div className="flex justify-between tablet:text-[14px] mobile:text-[12px]">
-                <p className="flex gap-[10px]">
-                  <span className="text-white01">{review.author}</span>
-                  <span className="text-gray03">{review.date}</span>
-                </p>
-                <div className="text-white03 flex gap-[20px]">
-                  <p className="underline">답글달기</p>
-                  <p>
-                    <span className="pr-[5px]">편집</span>|
-                    <span className="pl-[5px]">삭제</span>
-                  </p>
-                </div>
-              </div>
-
-              <hr className="h-[1px] border-gray02" />
-            </div>
+          {reviews.map((review, index) => (
+            <Review review={review} key={index} />
           ))}
         </div>
       </div>
