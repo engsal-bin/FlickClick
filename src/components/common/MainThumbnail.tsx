287db8316b6d2eee8100f43d3c9d24fd1c983c1e
songwon-sung <swsung91@gmail.com>
2025-03-30 18:46:37 +0900

    bug: 원형토글오류 해결 및 모바일 디바이스 간격 오류 해결

--- src/components/common/MainThumbnail.tsx
+++ src/components/common/MainThumbnail.tsx
@@ -5,6 +5,7 @@ import { movieAPI } from "../../api/movie";
 import { IMAGE_BASE_URL } from "../../api/axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css"; // Swiper 스타일 import
+import { Pagination } from "swiper/modules";
 
 export default function MainThumbnail() {
   const [contents, setContents] = useState<trendContentType[]>();
@@ -36,10 +37,7 @@ export default function MainThumbnail() {
         if (location.pathname === "/") {
           setContents(
             trendSum
-              .filter(
-                (item: trendContentType) =>
-                  item.backdrop_path !== null && item.overview !== ""
-              )
+              .filter((item: trendContentType) => item.backdrop_path !== null)
               .slice(0, 5)
           );
 
@@ -73,167 +71,172 @@ export default function MainThumbnail() {
     fetchData();
   }, [location.pathname]);
 
+  // swiperRef.current가 초기화되면 activeIndex로 currentIndex 설정
+  useEffect(() => {
+    if (swiperRef.current) {
+      setCurrentIndex(swiperRef.current.activeIndex);
+    }
+  }, [swiperRef.current]);
+
   return (
-    /* 임시로 시리즈 상세 페이지에 연결함 */
     <>
       {/* tablet 이상 */}
-      <Swiper
-        spaceBetween={10} // 슬라이드 사이 간격
-        slidesPerView={1} // 한 번에 보여지는 슬라이드 수
-        loop={true} // 무한 반복
-        className="w-full hidden tablet:flex"
-        onSlideChange={(swiper) => {
-          swiperRef.current = swiper;
-          setCurrentIndex(swiper.realIndex || 0);
-        }}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-      >
-        {isLoading
-          ? Array.from({ length: 5 }).map((_, index) => (
-              <SwiperSlide key={index}>
-                <div className="animate-pulse w-full mobile:h-[420px] tablet:h-[700px] bg-gray-700 ">
-                  <div
-                    className="w-full h-full flex flex-col justify-end items-start 
+      {isLoading ? (
+        <div className="animate-pulse w-full mobile:h-[420px] tablet:h-[700px] bg-gray-700 ">
+          <div
+            className="w-full h-full flex flex-col justify-end items-start 
                     gap-[10px] relative z-10 text-white bg-gray-700 rounded-md py-[80px] px-[100px]"
-                  >
-                    <div className="w-full h-[50px] bg-gray-600 rounded-md"></div>
-                    <div className="w-full h-[150px] bg-gray-600 rounded-md"></div>
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))
-          : contents?.map((content) => (
-              <SwiperSlide key={content.id}>
-                <Link
-                  key={content.id}
-                  to={
-                    content.title
-                      ? `/detailmovie/${content.id}`
-                      : `/detailseries/${content.id}`
-                  }
-                  className="relative hidden tablet:flex flex-col justify-center 
+          >
+            <div className="w-full h-[50px] bg-gray-600 rounded-md"></div>
+            <div className="w-full h-[150px] bg-gray-600 rounded-md"></div>
+          </div>
+        </div>
+      ) : (
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={10} // 슬라이드 사이 간격
+          slidesPerView={1} // 한 번에 보여지는 슬라이드 수
+          loop={true} // 무한 반복
+          className="w-full hidden tablet:flex"
+          pagination={{
+            clickable: true,
+            renderBullet: (index, className) => {
+              return `<button class="${className} transition-all 
+            ${index === currentIndex ? "bg-main" : "bg-white01_30"}"></button>`;
+            },
+          }}
+          onSlideChange={(swiper) => {
+            swiperRef.current = swiper;
+            setCurrentIndex(swiper.realIndex);
+          }}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+            setCurrentIndex(swiper.realIndex);
+          }}
+        >
+          {contents?.map((content) => (
+            <SwiperSlide key={content.id}>
+              <Link
+                key={content.id}
+                to={
+                  content.title
+                    ? `/detailmovie/${content.id}`
+                    : `/detailseries/${content.id}`
+                }
+                className="relative hidden tablet:flex flex-col justify-center 
               items-center w-full h-[720px] bg-cover bg-center py-[80px] px-[100px]"
-                  style={{
-                    backgroundImage: `url(${IMAGE_BASE_URL}original${content.backdrop_path})`,
-                  }}
-                >
-                  {/* 그라데이션 오버레이 */}
-                  <div
-                    className="absolute inset-0 bg-gradient-to-t from-black 
+                style={{
+                  backgroundImage: `url(${IMAGE_BASE_URL}original${content.backdrop_path})`,
+                }}
+              >
+                {/* 그라데이션 오버레이 */}
+                <div
+                  className="absolute inset-0 bg-gradient-to-t from-black 
               via-black/50 to-transparent opacity-100"
-                  ></div>
+                ></div>
 
-                  {/* 텍스트 */}
-                  <div
-                    className="w-full h-full flex flex-col justify-end items-start 
+                {/* 텍스트 */}
+                <div
+                  className="w-full h-full flex flex-col justify-end items-start 
                     gap-[10px] relative z-10 text-white"
-                  >
-                    {/* 제목 */}
-                    <div className="font-bold text-[60px] leading-none">
-                      {content.title ? content.title : content.name}
-                    </div>
+                >
+                  {/* 제목 */}
+                  <div className="font-bold text-[60px] leading-none">
+                    {content.title ? content.title : content.name}
+                  </div>
 
-                    {/* 오버뷰 */}
-                    <div
-                      className="text-[24px] leading-[40px] 
+                  {/* 오버뷰 */}
+                  <div
+                    className="text-[24px] leading-[40px] 
                   line-clamp-2 overflow-hidden whitespace-normal 
                   hover:line-clamp-none hover:overflow-visible"
-                    >
-                      {content.overview}
-                    </div>
+                  >
+                    {content.overview}
                   </div>
-                </Link>
-              </SwiperSlide>
-            ))}
-
-        {/* 원형 토글 */}
-        <div className="relative bottom-0 tablet:flex mobile:hidden justify-center left-1/2 transform -translate-x-1/2 gap-[10px] z-50">
-          {contents?.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => {
-                if (swiperRef.current) {
-                  swiperRef.current.slideToLoop(index);
-                }
-              }}
-              className={`w-[10px] h-[10px] rounded-full transition-all ${
-                currentIndex === index ? "bg-main" : "bg-white01_30"
-              }`}
-            ></button>
+                </div>
+              </Link>
+            </SwiperSlide>
           ))}
-        </div>
-      </Swiper>
+        </Swiper>
+      )}
 
       {/* mobile 전용 */}
-      <Swiper
-        spaceBetween={10} // 슬라이드 사이 간격
-        slidesPerView={1} // 한 번에 보여지는 슬라이드 수
-        loop={true} // 무한 반복
-        className="w-full flex tablet:hidden"
-        onSlideChange={(swiper) => {
-          swiperRef.current = swiper;
-          setCurrentIndex(swiper.realIndex || 0);
-        }}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-      >
-        {contents?.map((content) => (
-          <SwiperSlide key={content.id}>
-            <Link
-              key={content.id}
-              to={
-                content.title
-                  ? `/detailmovie/${content.id}`
-                  : `/detailseries/${content.id}`
-              }
-              className="relative tablet:hidden flex flex-col justify-center 
+      {isLoading ? (
+        <div className="animate-pulse w-full mobile:flex tablet:hidden">
+          <div className="w-full h-[420px] bg-gray-700 flex flex-col justify-end items-start gap-[10px] relative z-10 text-white bg-gray-700 rounded-md py-[50px] px-[30px]">
+            <div className="w-full h-[40px] bg-gray-600 rounded-md"></div>
+            <div className="w-full h-[120px] bg-gray-600 rounded-md"></div>
+          </div>
+        </div>
+      ) : (
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={10} // 슬라이드 사이 간격
+          slidesPerView={1} // 한 번에 보여지는 슬라이드 수
+          loop={true} // 무한 반복
+          className="w-full hidden mobile:flex"
+          pagination={{
+            clickable: true,
+            renderBullet: (index, className) => {
+              return `<button class="${className} transition-all 
+            ${index === currentIndex ? "bg-main" : "bg-white01_30"}"></button>`;
+            },
+          }}
+          onSlideChange={(swiper) => {
+            swiperRef.current = swiper;
+            setCurrentIndex(swiper.realIndex);
+          }}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+            setCurrentIndex(swiper.realIndex);
+          }}
+        >
+          {contents?.map((content) => (
+            <SwiperSlide key={content.id}>
+              <Link
+                key={content.id}
+                to={
+                  content.title
+                    ? `/detailmovie/${content.id}`
+                    : `/detailseries/${content.id}`
+                }
+                className="relative tablet:hidden flex flex-col justify-center 
               items-center w-full h-[420px] 
               bg-cover bg-center py-[50px] px-[30px]"
-              style={{
-                backgroundImage: `url(${IMAGE_BASE_URL}original${content.backdrop_path})`,
-              }}
-            >
-              {/* 그라데이션 오버레이 */}
-              <div
-                className="absolute inset-0 bg-gradient-to-t from-black 
-                via-transparent to-transparent opacity-100"
-              ></div>
-
-              {/* 텍스트 */}
-              <div
-                className="w-full h-full flex flex-col justify-end items-start 
-                gap-[10px] relative z-10 text-white"
+                style={{
+                  backgroundImage: `url(${IMAGE_BASE_URL}original${content.backdrop_path})`,
+                }}
               >
-                {/* 제목 */}
-                <div className="font-bold text-[36px] leading-none">
-                  {content.title ? content.title : content.name}
-                </div>
-
-                {/* 오버뷰 */}
+                {/* 그라데이션 오버레이 */}
                 <div
-                  className="text-[16px] leading-[20px]
+                  className="absolute inset-0 bg-gradient-to-t from-black 
+                via-transparent to-transparent opacity-100"
+                ></div>
+
+                {/* 텍스트 */}
+                <div
+                  className="w-full h-full flex flex-col justify-end items-start 
+                gap-[10px] relative z-10 text-white"
+                >
+                  {/* 제목 */}
+                  <div className="font-bold text-[36px] leading-none">
+                    {content.title ? content.title : content.name}
+                  </div>
+
+                  {/* 오버뷰 */}
+                  <div
+                    className="text-[16px] leading-[20px]
                   line-clamp-2 overflow-hidden whitespace-normal 
                   hover:line-clamp-none hover:overflow-visible"
-                >
-                  {content.overview}
+                  >
+                    {content.overview}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-
-        {/* 원형 토글 */}
-        <div className="relative bottom-0 tablet:hidden mobile:flex justify-center left-1/2 transform -translate-x-1/2 gap-[10px] z-50">
-          {contents?.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => swiperRef.current?.slideTo(index)} // 클릭 시 해당 인덱스로 이동
-              className={`w-[10px] h-[10px] rounded-full transition-all ${
-                currentIndex === index ? "bg-main" : "bg-white01_30"
-              }`}
-            ></button>
+              </Link>
+            </SwiperSlide>
           ))}
-        </div>
-      </Swiper>
+        </Swiper>
+      )}
     </>
   );
 }
