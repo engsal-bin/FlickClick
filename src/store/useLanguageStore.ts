155e8088f69c6678b8322ca2ff7a8729cbfa35df
engsal-bin <imaria0218@gmail.com>
2025-04-03 12:57:56 +0900

    feat: 언어지원 persist 적용

--- src/store/useLanguageStore.ts
+++ src/store/useLanguageStore.ts
@@ -1,13 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-type Language = 'ko' | 'en';
+type Language = "ko" | "en";
 
 interface LanguageStore {
   language: Language;
   setLanguage: (lang: Language) => void;
 }
 
-export const useLanguageStore = create<LanguageStore>((set) => ({
-  language: 'ko',
-  setLanguage: (lang) => set({ language: lang }),
-})); 
+export const useLanguageStore = create<LanguageStore>()(
+  persist(
+    (set) => ({
+      language: "ko",
+      setLanguage: (lang) => set({ language: lang }),
+    }),
+    {
+      name: "language-storage", // localStorage에 저장될 키
+    }
+  )
+);
