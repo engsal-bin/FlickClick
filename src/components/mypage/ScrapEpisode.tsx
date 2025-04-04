2576a680a4abb0ec32e0734288d67e93b2cdf492
songwon-sung <swsung91@gmail.com>
2025-04-04 16:18:38 +0900

    fix: 스크랩에피소드 ip_id 수정

--- src/components/mypage/ScrapEpisode.tsx
+++ src/components/mypage/ScrapEpisode.tsx
@@ -10,7 +10,7 @@ export default function ScrapEpisode({
       {episodeClips?.map((clip) => (
         <div key={clip.ip_id}>
           <EpisodeContent
-            ip_id={`detailepisode/${clip.ip_id}`}
+            ip_id={`${clip.ip_id}`}
             ip_name={clip.ip_name}
             poster_path={clip.poster_path}
             summary={clip.summary}
